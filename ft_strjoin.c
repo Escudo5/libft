b143d46a11286e795de60dc8b683eef9584ba739
Escudo5 <sergiomargar03@gmail.com>
2024-09-23 16:46:55 +0200

    ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: smarquez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 16:07:56 by smarquez          #+#    #+#             */
-/*   Updated: 2024/09/23 16:12:26 by smarquez         ###   ########.fr       */
+/*   Updated: 2024/09/23 16:44:06 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,35 @@
 
 char *ft_strjoin(char const *s1, char const *s2)
 {
+	if ((s1 == NULL || s2 == NULL) )
+		return(NULL);
 
+	size_t s1_len;
+	size_t s2_len;
+
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+	
+	joined= (char*)malloc((s1_len + s2_len + 1) * sizeof (char));
+	
+	if (joined == NULL)
+		return(NULL);
+	i = 0;
+	while (i < s1_len)
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+
+	j = 0;
+
+	while (j < s2_len)
+	{
+		joined[i + j] = s2[j];
+		j++;
+	}
+
+	joined[i + j] = '\0';
+
+	return (joined);
+}
