7bb8648109e1133a31d8e74b156d83f7e5fa8408
Escudo5 <sergiomargar03@gmail.com>
2024-09-24 15:36:24 +0200

    main bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smarquez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 13:21:08 by smarquez          #+#    #+#             */
-/*   Updated: 2024/09/17 13:35:25 by smarquez         ###   ########.fr       */
+/*   Updated: 2024/09/24 15:13:54 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void bzero(void *s, size_t n)
+void ft_bzero(void *s, size_t n)
 {
 	size_t	i;
 	unsigned char	*ptr;
@@ -24,4 +24,16 @@ void bzero(void *s, size_t n)
 		ptr[i] = 0;
 		i++;
 	}
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char str[50] = "Hello, World!";
+	printf("Before bzero: %s\n", str);
+	ft_bzero(str, 5);
+	printf("After bzero: %s\n", str);
+	return (0);
 }
