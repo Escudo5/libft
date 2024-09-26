d0cb075764b839e4a4719e352703333ba43bae7e
Escudo5 <sergiomargar03@gmail.com>
2024-09-26 11:50:44 +0200

    ft_putnbr_fd

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/26 11:38:23 by smarquez          #+#    #+#             */
+/*   Updated: 2024/09/26 11:50:34 by smarquez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stddef.h>
+#include <unistd.h>
+
+void ft_putnbr_fd(int n, int fd)
+{
+  if (n < 0)
+   {
+        ft_putchar_fd('-', fd);
+        n = -n;
+   }
+    if (n == 0)
+    {
+        ft_putchar_fd('0',fd);
+        return;
+    }
+    if (n > 0)
+    {
+        ft_putnbr_fd(n / 10, fd);
+        ft_putnbr_fd(n % 10 + '0', fd);
+    }
+}
