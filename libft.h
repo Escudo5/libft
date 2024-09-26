2215a61d79208b78ab0829eda55b8f3622986089
Escudo5 <sergiomargar03@gmail.com>
2024-09-26 16:11:57 +0200

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 10:31:07 by smarquez          #+#    #+#             */
-/*   Updated: 2024/09/26 16:03:11 by smarquez         ###   ########.fr       */
+/*   Updated: 2024/09/26 16:11:38 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ char    *ft_strrchr(const char *s, int c);
 int     ft_strncmp(const char *s1, const char *s2, size_t n);
 void    *ft_memchr(const void *s, int c, size_t n);
 int     ft_memcmp(const void *s1, const void *s2, size_t n);
-char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
+char    *ft_strnstr(const char *big, const char *little, size_t len);
 int     ft_atoi(const char *str);
 void    *ft_calloc(size_t count, size_t size);
 char    *ft_strdup(const char *s1);
