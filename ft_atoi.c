fe57697090d362350741ccdfab139a36cec61a3d
Escudo5 <sergiomargar03@gmail.com>
2024-09-26 12:28:57 +0200

    main aatoi y memset

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smarquez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 15:09:06 by smarquez          #+#    #+#             */
-/*   Updated: 2024/09/19 15:27:40 by smarquez         ###   ########.fr       */
+/*   Updated: 2024/09/26 12:21:33 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,31 @@ int ft_atoi(const char *nptr)
 	int sign;
 	sign = 1;
 	if (*nptr == '-')
+	{	
 		sign = -1;
 		nptr++;
+	}
 	if (*nptr == '+')
+	{	
 		nptr++;
-
+	}
 	int result;
 	result = 0;
-
 	while (*nptr >= '0' && *nptr <= '9')
 	{
 		result = result * 10 + (*nptr - '0');
 		nptr++;
 	}
-	return (sign + result);
+	return (sign * result);
+}
+
+#include <stdio.h>
+
+int main()
+{
+	const char s1[] = "awsjj8675kbddw";
+	int result;
+	result = ft_atoi(s1);
+	printf("%d", result);
+	return (0);
 }
--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 11:59:07 by smarquez          #+#    #+#             */
-/*   Updated: 2024/09/24 12:33:32 by smarquez         ###   ########.fr       */
+/*   Updated: 2024/09/26 12:28:25 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,17 @@ void	*ft_memset(void *s, int c, size_t n)
 	return (s);
 }
 
+#include <stdio.h>
+
+int main()
+{
+	char s1[] = "hhyuuyvkyvk";
+	int c1;
+	c1 = 77;
+	size_t n1;
+	n1 = 6;
+	
+	ft_memset(s1, c1, n1);
+	printf("%s", s1);
+	return (0);
+}
