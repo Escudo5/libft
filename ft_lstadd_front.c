0c39f4bf07fe5b59ba936e744701fc3a2e0f307a
Escudo5 <sergiomargar03@gmail.com>
2024-09-30 14:00:49 +0200

    listas y nodos

---
+++ ft_lstadd_front.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 12:56:43 by smarquez          #+#    #+#             */
+/*   Updated: 2024/09/30 13:47:51 by smarquez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
---
+++ ft_lstlast.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 13:52:04 by smarquez          #+#    #+#             */
+/*   Updated: 2024/09/30 14:00:39 by smarquez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+t_list *ft_lstlast(t_list *lst)
+{
+    if (lst == NULL)
+        return NULL;
+    while (lst->next != NULL)
+    {
+        lst = lst -> next;
+    }
+    return (lst);
+}
