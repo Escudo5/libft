30af9a8d61caed2ef815bc98b1acb2f2bb62ebbf
Escudo5 <sergiomargar03@gmail.com>
2024-10-02 10:12:28 +0200

    correccion strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 15:24:41 by smarquez          #+#    #+#             */
-/*   Updated: 2024/10/01 10:55:15 by smarquez         ###   ########.fr       */
+/*   Updated: 2024/10/02 10:06:36 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strrchr(const char *s, int c)
 			last_occurrence = (char *)s;
 		s++;
 	}
-	if (c == '\0')
+	if ((char)c == '\0')
 		return ((char *)s);
 	return (last_occurrence);
 }
